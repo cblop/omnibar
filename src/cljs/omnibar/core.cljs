899f8ff2ac39b86369569433bfafb4aca609c4f1
East <eastric@uk.experian.local>
2016-02-24 22:19:39 +0000

    displaying full accordion keyboard

--- src/cljs/omnibar/core.cljs
+++ src/cljs/omnibar/core.cljs
@@ -2,24 +2,16 @@
     (:require [reagent.core :as reagent :refer [atom]]
               [reagent.session :as session]
               [secretary.core :as secretary :include-macros true]
-              [accountant.core :as accountant]))
-
+              [accountant.core :as accountant]
+              [omnibar.accordion :as accord]))
 
 ;; -------------------------
 ;; svg
-(defn circle [[x y]]
-  [:circle {:cx (if (even? y)
-                  (+ 60 (* x 48))
-                  (+ 35 (* x 48)))
-            :cy (+ 30 (* y 48))
-            :class "circle"
-            :r "19"
-;;            :stroke "#00f"
-;;            :stroke-width "2"
-;;            :fill "#00eb00"
-            }])
 
-(defn hexagon [[x y]]
+
+
+
+(defn inkscape-hexagon [[x y]]
   (let [hx (if (even? y)
             (+ 90 (* x 50))
             (+ 65 (* x 50)))            
@@ -46,7 +38,6 @@
                   :stroke-width "1px"
                   :fill "#fff"}
           :d (str "m" x " " y "v51l29 17 29-17v-35.858l-29-17.142-0.004-52.844-28.797 0.0101z")}]])
-
 
 (defn defs-piano-key-b-shape
   "define a svg piano key. svg use can access it. As an example, this is the shape used by middle c"
@@ -76,18 +67,7 @@
 
    ;; may wan to revisit this using the use tag and defs
    ;; this may reduce the amount of code sent to the client
-   (map #(hexagon %) (grid 9 5))])
-
-(defn svg-circles [h w]
-  [:svg {:width w
-         :height h
-         :id "circles"
-   :style {:outline "2px solid black"
-           :background-color "#eee"}}
-
-   ;; may wan to revisit this using the use tag.
-   ;; this may reduce the amount of code sent to the client
-   (map #(circle %) (grid 9 5))])
+   (map #(inkscape-hexagon %) (grid 9 5))])
 
 (defn svg-piano-keyboard [h w]
   [:svg {:width w
@@ -98,70 +78,52 @@
    (defs-piano-key-b-shape)
    [:use {:xlink/href "#keybshape" :x 100 :y 10}]
 
-
    (piano-key-b-shape [40 60] 1)
    (piano-key-b-shape [-200 60] -1)
    (piano-key-b-shape [200 60] 1)
    (piano-key-b-shape [-400 60] -1)
-   
    ])
 
 
-
-(defn svg-triangles []
-  [:svg {:width "500px" :height "250px" :viewBox "0 0 200 200"
-         :xmlns "http://www.w3.org/2000/svg"
-         :xmlns/xlink="http://www.w3.org/1999/xlink"
-
-         :version "1.1"
-         :style {:outline "2px solid black"
-                 :background-color "#eee"}}
-   [:defs 
-    [:g {:id "simple-circle"}
-     [:circle {:cx 110 :cy 100 :class "circle" :r "19"}]]]
-   [:use {:xlink:href "#simple-circle"} ]]
-   )
-
 ;; All the points to make up a polygon
+(def hexagon-points "20.000,0.000 10.000,17.321 -10.000,17.321 -20.000,0.000 -10.000,-17.321 10.000,-17.321")
 
-(def polygon-points "20.000,0.000 10.000,17.321 -10.000,17.321 -20.000,0.000 -10.000,-17.321 10.000,-17.321")
+(def hexagon
+  [:polygon {:transform "rotate(-30),scale(1.6,1.6)"
+             :fill "hsl(20, 10%, 95%)"
+             :stroke "hsl(0, 0%, 70%)"
+             :stroke-width "0.5" 
+             :points hexagon-points}] )
 
-(defn polygon-tile [[x y] title]
-  (let [hx (if (even? y)
-            (+ 28 (* x 55))
-            (+ 55.5 (* x 55)))            
-        hy   (+ 35 (* y 48))]
+(def circle
+  [:circle {:r "27" :stroke "black" :stroke-width "1" :fill "red"}] )
 
-    [:g {:class "tile" :transform (str "translate(" hx "," hy ")")}
-     [:polygon {:transform "rotate(-30),scale(1.6,1.6)"
-                :fill "hsl(20, 10%, 95%)"
-                :stroke "hsl(0, 0%, 70%)"
-                :stroke-width "0.5" 
-                :points polygon-points}]
+(defn accordion-keyboard [shape note]
+  (let [[x y] (:position note)
+        title (:text note)
+        y-start-pos 33
+        x-start-pos 33
+        hx   (+ y-start-pos (* x 48))
+        hy   (+ x-start-pos (* x 27) (* y 55))]
+
+    [:g {:class "tile" :transform (str "translate(" hy "," hx ")")}
+     shape 
      [:text {:y "0.4em"
              :transform "scale(.95)"
              :text-anchor "middle"}
-      title]])
+      title]]))
 
-  )
+(defn accordion-group [shape note-group]
+  (map #(accordion-keyboard shape %) (:notes note-group)) )
 
-(defn svg-accordion [h w]
+(defn svg-accordion [[h w] shape]
     [:svg {:width w
            :height h
            :id "accordion"
            :viewBox (str "0 0 100% " h)
    :style {:outline "2px solid black"
            :background-color "#eee"}}
-     (map #(polygon-tile %1 %2) (grid 12 2)
-          ["D♭"  "A♭" "E♭" "B♭" "F"  "C"  "G"  "D"  "A" "E" "B" "F♯"
-           "B♭♭" "F♭" "C♭" "G♭" "Db" "A♭" "E♭" "B♭" "F" "C" "G" "D"
-           ])
-     ])
-
-
-
-;;How do you apply use tag 
-
+     (map #(accordion-group shape %) accord/stradella-bass) ])
 
 ;; -------------------------
 ;; Views
@@ -176,16 +138,14 @@
         ".tile polygon { pointer-events: visiblePainted;  fill: hsl(60, 10%, 95%); stroke: hsl(0, 0%, 70%);  stroke-width: 0.5;     }"
         ".tile text { font-family: \"Droid Sans\";     }")]
   
-   [:div [:h2 "Welcome to omnibar"]
+  [:div
+   [:h2 "Welcome to omnibar"]
    [:div [:a {:href "/about"} "go to about page"]]
-
    [:div
-   [svg-inkscape-hexagons 250 500]
-   [svg-circles 250 500]
-   [svg-triangles] 
-    [svg-piano-keyboard 150 500]]
-    [svg-accordion 150 "100%"]
-   ])
+    [svg-piano-keyboard 150 800]
+    [svg-accordion [320 1250] hexagon]
+    [svg-accordion [320 1250] circle]
+    [svg-inkscape-hexagons 250 500]]    ])
 
 (defn about-page []
   [:div [:h2 "About omnibar"]
