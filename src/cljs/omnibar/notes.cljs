2f76bfc661481ab6c4bce6ef2e0debf197d39502
East <eastric@uk.experian.local>
2016-03-08 21:58:02 +0000

    Refactored midi/notes, to make it smaller

--- src/cljs/omnibar/notes.cljs
+++ src/cljs/omnibar/notes.cljs
@@ -2,18 +2,25 @@
 
 ;; Overtone has a more robust set of functions. We don't need it's level of functionality yet.
 ;; https://github.com/overtone/overtone/blob/8b8353b27c755004dc0733857bcfee9ce15c6b73/src/overtone/music/pitch.clj
-(def note-midi-relationship {                                                 :A0  21 :A#0  22 :B0  23
-     :C1  24 :C#1 25 :D1 26 :D#1 27 :E1  28 :F1  29 :F#1  30 :G1  31 :G#1  32 :A1  33 :A#1  34 :B1  35
-     :C2  36 :C#2 37 :D2 38 :D#2 39 :E2  40 :F2  41 :F#2  42 :G2  43 :G#2  44 :A2  45 :A#2  46 :B2  47  
-     :C3  48 :C#3 49 :D3 50 :D#3 51 :E3  52 :F3  53 :F#3  54 :G3  55 :G#3  56 :A3  57 :A#3  58 :B3  59  
-     :C4  60 :C#4 61 :D4 62 :D#4 63 :E4  64 :F4  65 :F#4  66 :G4  67 :G#4  68 :A4  69 :A#4  70 :B4  71  
-     :C5  72 :C#5 73 :D5 74 :D#5 75 :E5  76 :F5  77 :F#5  78 :G5  79 :G#5  80 :A5  81 :A#5  82 :B5  83  
-     :C6  84 :C#6 85 :D6 86 :D#6 87 :E6  88 :F6  89 :F#6  90 :G6  91 :G#6  92 :A6  93 :A#6  94 :B6  95
-     :C7  96 :C#7 97 :D7 98 :D#7 99 :E7 100 :F7 101 :F#7 102 :G7 103 :G#7 104 :A7 105 :A#7 106 :B7 107 
-     :C8 108})
+
+(def lowest-piano-midi 21)
+(def highest-piano-midi 108)
+
+(def piano-notes [                                        :A0 :A#0 :B0 
+                  :C1 :C#1 :D1 :D#1 :E1 :F1 :F#1 :G1 :G#1 :A1 :A#1 :B1 
+                  :C2 :C#2 :D2 :D#2 :E2 :F2 :F#2 :G2 :G#2 :A2 :A#2 :B2  
+                  :C3 :C#3 :D3 :D#3 :E3 :F3 :F#3 :G3 :G#3 :A3 :A#3 :B3  
+                  :C4 :C#4 :D4 :D#4 :E4 :F4 :F#4 :G4 :G#4 :A4 :A#4 :B4  
+                  :C5 :C#5 :D5 :D#5 :E5 :F5 :F#5 :G5 :G#5 :A5 :A#5 :B5  
+                  :C6 :C#6 :D6 :D#6 :E6 :F6 :F#6 :G6 :G#6 :A6 :A#6 :B6 
+                  :C7 :C#7 :D7 :D#7 :E7 :F7 :F#7 :G7 :G#7 :A7 :A#7 :B7 
+                  :C8])
+
+(def note-midi-map (zipmap piano-notes
+                           (range lowest-piano-midi (inc highest-piano-midi))))
 
 (defn note->midi [notename]
-  (get note-midi-relationship (keyword notename)))
+  (get note-midi-map (keyword notename)))
 
 (defn midi->freq [note-num]
   (let [expt-numerator (- note-num 49)
